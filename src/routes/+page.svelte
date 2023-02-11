f5ba20a519e87b2a0bfd0f83feaa22b98ec76395
Dominik Lenz <dominik@notabot.cc>
2023-02-11 15:46:17 +0100

    adds home button on dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <Navbar user={data.user} title="Home"/>
-<div class="w-screen h-screen flex flex-col justify-center items-center text-white ">
+<div class="w-screen h-screen flex flex-col justify-center items-center text-white">
     <p class="text-xl">This ist the Homepage</p>
     <a href="/dashboard" class="mt-16 font-bold text-2xl p-2 rounded-md bg-teal-800">Dashboard</a>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <Navbar user="{data.user}" title="Dashboard"/>
-<div class="w-screen h-screen flex justify-center items-center text-white font-bold text-2xl">
-    Dashboard (only visible whe logged in)
+<div class="w-screen h-screen flex flex-col justify-center items-center text-white">
+    <p class="text-xl"> Dashboard (only visible whe logged in)</p>
+    <a href="/" class="mt-16 font-bold text-2xl p-2 rounded-md bg-teal-800">Home</a>
 </div>
