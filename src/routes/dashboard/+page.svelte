2606fc6ebe8c8d0e0e9b06636c3aef61beb3b490
Dominik Lenz <dominik@notabot.cc>
2023-02-11 16:21:03 +0100

    fixes typo

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -6,6 +6,6 @@
 
 <Navbar user="{data.user}" title="Dashboard"/>
 <div class="w-screen h-screen flex flex-col justify-center items-center text-white">
-    <p class="text-xl"> Dashboard (only visible whe logged in)</p>
+    <p class="text-xl"> Dashboard (only visible when logged in)</p>
     <a href="/" class="mt-16 font-bold text-2xl p-2 rounded-md bg-teal-800">Home</a>
 </div>
