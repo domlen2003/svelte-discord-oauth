f3da3d200556ece57e4bd77869120d40d9888eba
Dominik Lenz <dominik@notabot.cc>
2023-03-27 16:21:17 +0200

    adds already-logged-in redirect

--- src/routes/api/discord/login/+server.ts
+++ src/routes/api/discord/login/+server.ts
@@ -1,10 +1,11 @@
-import {DISCORD_CLIENT_ID,DISCORD_REDIRECT_URI} from "$env/static/private";
+import {DISCORD_CLIENT_ID, DISCORD_REDIRECT_URI} from "$env/static/private";
 import type {RequestHandler} from '@sveltejs/kit';
+
 const DISCORD_ENDPOINT = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&response_type=code&scope=identify%20email`;
 
-export const GET: RequestHandler = async (_) => {
+export const GET: RequestHandler = async ({locals}) => {
     return new Response(null, {
-        headers: {Location: DISCORD_ENDPOINT},
+        headers: {Location: locals.user ? "/dashboard" : DISCORD_ENDPOINT},
         status: 302
     })
 }
