e03429dbf9dea7da6cb521e2350561052e826bf1
Dominik Lenz <dominik@notabot.cc>
2023-03-27 16:39:34 +0200

    removes unnecessary api route

--- src/lib/server/discordApi.ts
+++ src/lib/server/discordApi.ts
@@ -1,6 +1,7 @@
 import type {RequestEvent} from "@sveltejs/kit";
 import {DISCORD_API_URL} from "$env/static/private";
 import type {DiscordUser} from "../types/discord";
+import {buildSearchParams, requestDiscordToken, setCookies} from "../../routes/api/discord/discordAuth";
 
 export async function authenticateUser(event: RequestEvent): Promise<DiscordUser | null> {
     const token = await getOrRefreshToken(event);
@@ -23,9 +24,9 @@ async function getOrRefreshToken(event: RequestEvent): Promise<string | null> {
     // fetch refresh only if not already on refresh route (otherwise recursion go brrr)
     const refreshToken = event.cookies?.get('discord_refresh_token')
     if (refreshToken && !event.url.pathname.startsWith('/api/discord/refresh')) {
-        const discord_request = await event.fetch(`api/discord/refresh?code=${refreshToken}`)
-        const discord_response = await discord_request.json()
-        return discord_response.token
+        const tokens = await requestDiscordToken(buildSearchParams("identify email", "refresh", refreshToken));
+        setCookies(tokens, event.cookies)
+        return tokens.access_token
     }
 
     return null
